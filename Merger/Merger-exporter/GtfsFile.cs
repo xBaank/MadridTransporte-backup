fcc1a7a1f0f22ec014b59d3ac53e675b276d8235
Roberto Blázquez <elroby444@gmail.com>
2025-03-18 13:59:43 +0100

    wip

---
+++ Merger/Merger-exporter/GtfsFile.cs
@@ -0,0 +1,75 @@
+﻿using System.IO.Compression;
+using System.Runtime.CompilerServices;
+
+namespace Merger_exporter;
+
+internal class GtfsFile(string name, Stream stream) : IAsyncDisposable
+{
+    private event Action? DisposeFiles;
+
+    private async ValueTask<FileInfo> DownloadAsync(CancellationToken cancellationToken)
+    {
+        var tempFileName = Path.GetTempFileName();
+        var tempFile = new FileInfo(tempFileName);
+        using var fileStream = tempFile.Open(FileMode.Create);
+        Console.WriteLine($"Using temp file {tempFileName}");
+        await stream.CopyToAsync(fileStream, cancellationToken);
+        Console.WriteLine($"Downloaded {name} to {tempFileName}");
+        DisposeFiles += tempFile.Delete;
+        return tempFile;
+    }
+
+    private async ValueTask<string> UnzipFiles(CancellationToken cancellationToken)
+    {
+        var file = await DownloadAsync(cancellationToken);
+        var tempFolder = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+        await Task.Run(
+            () => ZipFile.ExtractToDirectory(file.FullName, tempFolder),
+            cancellationToken
+        );
+        Console.WriteLine($"Unzipped {file.FullName} to {tempFolder}");
+        DisposeFiles += () => Directory.Delete(tempFolder, true);
+        return tempFolder;
+    }
+
+    public async IAsyncEnumerable<string> GetFoldersAsync(
+        [EnumeratorCancellation] CancellationToken cancellationToken
+    )
+    {
+        var tempFolder = await UnzipFiles(cancellationToken);
+        foreach (var entry in Directory.EnumerateFileSystemEntries(tempFolder))
+        {
+            if (Directory.Exists(entry))
+            {
+                Console.WriteLine($"Folder: {entry}");
+
+                //Extract subGtfsFiles
+                foreach (var subGtfsFile in Directory.EnumerateFiles(entry))
+                {
+                    if (Path.GetExtension(subGtfsFile) != ".zip")
+                        continue;
+
+                    using var fileStream = File.Open(subGtfsFile, FileMode.Open);
+                    var gtfsFile = new GtfsFile(Path.GetFileName(subGtfsFile), fileStream);
+                    await foreach (var item in gtfsFile.GetFoldersAsync(cancellationToken))
+                    {
+                        yield return item;
+                    }
+                }
+            }
+            else if (File.Exists(entry))
+            {
+                //Leave as it is
+                Console.WriteLine($"File: {entry}");
+                yield return entry;
+            }
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        Console.WriteLine("Disposing...");
+        await stream.DisposeAsync();
+        DisposeFiles?.Invoke();
+    }
+}
---
+++ Merger/Merger-exporter/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Runtime.CompilerServices;
+
+using Merger_exporter;
+
+List<(string name, string url)> files =
+[
+    (
+        "google_transit_M4",
+        "https://www.arcgis.com/sharing/rest/content/items/5c7f2951962540d69ffe8f640d94c246/data"
+    ),
+    (
+        "google_transit_M6",
+        "https://www.arcgis.com/sharing/rest/content/items/868df0e58fca47e79b942902dffd7da0/data"
+    ),
+    (
+        "google_transit_M9",
+        "https://www.arcgis.com/sharing/rest/content/items/357e63c2904f43aeb5d8a267a64346d8/data"
+    ),
+    (
+        "google_transit_M89",
+        "https://www.arcgis.com/sharing/rest/content/items/885399f83408473c8d815e40c5e702b7/data"
+    ),
+    (
+        "google_transit_M10",
+        "https://www.arcgis.com/sharing/rest/content/items/aaed26cc0ff64b0c947ac0bc3e033196/data"
+    ),
+    (
+        "google_transit_M5",
+        "https://www.arcgis.com/sharing/rest/content/items/1a25440bf66f499bae2657ec7fb40144/data"
+    ),
+];
+
+var token = CreateGracefulCancelationToken();
+
+using var httpClient = new HttpClient();
+var gtfsFiles = await GetGtfsFilesAsync(token).ToListAsync();
+
+try
+{
+    foreach (var item in gtfsFiles)
+    {
+        var folders = await item.GetFoldersAsync(token).ToListAsync();
+    }
+}
+finally
+{
+    foreach (var item in gtfsFiles)
+    {
+        await item.DisposeAsync();
+    }
+}
+
+async IAsyncEnumerable<GtfsFile> GetGtfsFilesAsync(
+    [EnumeratorCancellation] CancellationToken cancellationToken
+)
+{
+    foreach (var (name, url) in files)
+    {
+        var gtfsFile = new GtfsFile(name, await httpClient.GetStreamAsync(url, token));
+        yield return gtfsFile;
+    }
+}
+
+static CancellationToken CreateGracefulCancelationToken()
+{
+    var cts = new CancellationTokenSource();
+    Console.CancelKeyPress += (s, e) =>
+    {
+        Console.WriteLine("Canceling...");
+        cts.Cancel();
+        e.Cancel = true;
+    };
+    return cts.Token;
+}
